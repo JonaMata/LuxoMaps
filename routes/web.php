977dd1d7829fbe3869464c267c168e5f3ddd1389
Jonathan Matarazzi <git@jonathanm.nl>
2025-02-12 23:35:39 +0100

    Update pik of bal url

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('auth')->group(function () {
             Route::get('create', 'create')->name('create');
             Route::post('store', 'store')->name('store');
         });
-        Route::get('ap', 'App\Http\Controllers\PikBalController@create')->name('ap');
+        Route::get('pikofbal', 'App\Http\Controllers\PikBalController@create')->name('pikofbal');
 
         Route::prefix('stickers')->name('stickers.')->controller(\App\Http\Controllers\StickerController::class)->group(function() {
             Route::post('', 'create')->name('create');
